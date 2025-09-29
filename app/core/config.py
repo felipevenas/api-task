abd284520dfe9bbd4144587805dab93bed493271
Felipe Venas <felipevenas3003@gmail.com>
2025-09-29 10:13:16 -0300

    Mudança do nome da rota de login

---
+++ app/core/config.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+def setup_cors(app: FastAPI):
+
+    origins = [
+    "http://127.0.0.1:5000",
+    "http://127.0.0.1:5000/register",
+    "http://127.0.0.1:5000/auth",
+    ]
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"], # Habilita todos os métodos HTTP
+        allow_headers={"*"}  # Permite todos os cabeçalhos
+        )
+    
+    print("CORS Middleware configurado com sucesso!")
