0c0e84e342e325396ec23c2b0aadf962b25ef46b
Felipe Venas <felipevenas3003@gmail.com>
2025-09-29 14:59:17 -0300

    Outras mudanças

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi.security import OAuth2PasswordBearer
+from typing import Annotated
 
 from app.core.config import setup_cors
 from app.domain.user.routes import user_router
@@ -7,6 +9,8 @@ from app.domain.customer.routes import customer_router
 
 app = FastAPI()
 setup_cors(app)
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='token')
 
 app.include_router(user_router)
 app.include_router(task_router)
